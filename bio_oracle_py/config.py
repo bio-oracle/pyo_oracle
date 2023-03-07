3bf96ef7d8b0646166592e707d1e25ddfe0fd2ec
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-03-07 17:48:55 +1100

    Finished config module

--- bio_oracle_py/config.py
+++ bio_oracle_py/config.py
@@ -1,38 +1,58 @@
 """
-Module to handle configuration, such as data dir, etc.
+Module to handle configuration, such as data directory, etc.
 """
 import configparser
 from pathlib import Path
 
 
-
-default_config = {
-    "local_data_directory": Path(__file__).joinpath("local_data"),
-    "config_file": Path(__file__).joinpath("local_data/config.ini")
+_default_config = {
+    "data_directory": str(Path(__file__).parent.joinpath("data/").absolute()),
+    "erddap_server": "http://erddap.bio-oracle.org:8080/erddap/"
 }
 
+config_file = Path(__file__).absolute().parent.parent.joinpath(("config.ini"))
 
-def create_config(default_config: dict, path: str = None):
+def create_config(default_config: dict = _default_config, path: str = None):
     """
     Creates the configuration file.
     """
-    config = configparser.ConfigParser("DEFAULT")
+    config = configparser.ConfigParser()
     config["DEFAULT"] = default_config
-    config_file = Path(config["DEFAULT"]["config_file"]) if not path else path
     if config_file.exists():
-        response = input(f"Config file '{config_file}' already exists, overwrite it? y/N")
-        if response.lower() not in "y yes":
-            print("Operation cancelled.")
+        response = input(f"Config file '{config_file}' already exists, overwrite it? y/N \n")
+        if (response.lower() not in "y yes") or (not response):
+            print("Operation cancelled.\n")
             return
     with open(config_file, "w") as f:
         config.write(f)
+        print(f"Created configuration at '{config_file}'.")
 
+
+def _get_default_config():
+    if config_file.exists():
+        current_config = configparser.ConfigParser()
+        _ = current_config.read(config_file)
+        return current_config
+    else:
+        try:
+            print("Config file doesn't exist, creating it.")
+            create_config()
+            return _get_default_config()
+        except:
+            config = configparser.ConfigParser()
+            config["DEFAULT"] = _default_config
+            return config
 
 
 def show_config_path():
     """
     Returns the path to the config file.
     """
+    if config_file.exists():
+        return config_file
+    else:
+        print("Config file doesn't exist, creating it.")
+        create_config()
     
 
 
@@ -40,13 +60,22 @@ def show_config_values():
     """
     Prints configuration values.
     """
-    pass
+    if not config_file.exists():
+        print("Config file doesn't exist, creating it.")
+        create_config()
+    print(f"Configuration values in '{config_file}' are as following:\n")
+    for key, value in _get_default_config()["DEFAULT"].items():
+        print(key, "\t", value)
+    print()
+    print(f"Edit the configuration file '{config_file}' or use the 'update_setting' function in this module to edit them.")
 
 
 def update_setting(key, value):
     """
     Modifies a setting in the configuration file.
     """
-    pass
-
-
+    config = _get_default_config()
+    config["DEFAULT"][key] = value
+    with open(config_file, "w") as f:
+        config.write(f)
+    print(f"Successfully updated config file at '{config_file}'.")
