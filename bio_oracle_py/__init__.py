c7964c000bb98a70cd5544a2a795ff719a85048c
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-03-07 18:43:41 +1100

    Write init module

--- bio_oracle_py/__init__.py
+++ bio_oracle_py/__init__.py
@@ -1,3 +1,4 @@
 from .main import download_layers, list_layers, list_local_data
+from .config import config
 
-__all__ = ["download_layers", "list_layers", "list_local_data"]
+__all__ = ["download_layers", "list_layers", "list_local_data", "config"]
