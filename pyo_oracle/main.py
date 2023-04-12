3e63ca414995613d94a6154cc66007870bd8ec3a
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-04-12 16:45:29 +1000

    A bunch of improvements
      - Refactor download_layers to use httpx
      - Add _format_args function
      - Improve list_layers function

--- pyo_oracle/main.py
+++ pyo_oracle/main.py
@@ -17,18 +17,51 @@ default_server = ERDDAP(
 )
 
 
-def download_layers(dataset_ids: str or list, response: str = "nc", constraints: dict = None):
+def _format_args(function):
+    """
+    Converts list/str to tuple.
+
+    Prevent TypeError when passing list to function with lru_cache.
+
+    From: https://stackoverflow.com/questions/49210801/python3-pass-lists-to-function-with-functools-lru-cache
+    """
+    def wrapper(*args, **kwargs):
+        kwargs = {k: tuple(x) if type(x) == list else x for k, x in kwargs.items()}
+        kwargs = {k: (x,) if type(x) == str else x for k, x in kwargs.items()}
+        args = [tuple(x) if type(x) == list else x for x in args]
+        args = [(x,) if type(x) == str else x for x in args]
+        result = function(*args, **kwargs)
+        return result
+    return wrapper
+
+
+def _validate_argument(name: str, value: str or list or tuple, valid_values: list):
+    """Check if argument is in a valid list of values."""
+    if value:
+        for v in value:
+            if v.lower() not in valid_values:
+                print(f"Selected {name} '{v}' is not a valid {name}. These are valid values:\n{valid_values}")
+
+
+def download_layers(dataset_ids: str or list, output_directory: str or Path = None, response: str = "nc", constraints: dict = None, skip_confirmation=False):
     """
     Downloads one or more layers.
     """
     if isinstance(dataset_ids, str):
-        dataset_ids = [dataset_ids,]
+        dataset_ids = (dataset_ids,)
+
+    if not skip_confirmation:
+        response = input("No constraints have been set. This will download the full dataset, which may be a few GBs in size. Would you like to proceed? y/N")
+        if response.lower() not in "y yes".split():
+            print("Download cancelled.")
+            return
 
     s = deepcopy(default_server)
     for dataset_id in dataset_ids:
-        url = _get_griddap_dataset_url(dataset_id, response=response, constraints=constraints, response=response)
+        url = _get_griddap_dataset_url(dataset_id, constraints=constraints, response=response)  # Response is converted to 
         filename = f"{dataset_id}.{response}"
-        local_path = Path(config["data_directory"]).joinpath(filename)
+        outdir = output_directory if output_directory else config["data_directory"]
+        local_path = Path(outdir).joinpath(filename)
         _download_file_from_url(url, local_path)
         
 
@@ -39,16 +72,13 @@ def _download_file_from_url(url: str, local_path: Path):
     From: https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
     """
     # NOTE the stream=True parameter below
-    with httpx.get(url, stream=True) as r:
-        r.raise_for_status()
-        print(f"Writing to '{local_path}'.\n")
-        with open(local_path, "wb") as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                # if chunk:
+    with open(local_path, "wb") as f:
+        with httpx.stream("GET", url) as response:
+            for chunk in response.iter_bytes():
                 f.write(chunk)
-    return local_path    
+    return local_path
+
+
 
 
 def _get_griddap_dataset_url(
@@ -103,25 +133,16 @@ def _layer_dataframe(include_allDatasets=False):
     return df
 
 
-def _validate_argument(name: str, value: str or list, valid_values: list):
-    """Check if argument is in a valid list of values."""
-    if value:
-        if isinstance(value, str):
-            value = [value,]
-        for v in value:
-            if v.lower() not in valid_values:
-                print(f"Selected {name} '{v}' is not a valid {name}. These are valid values:\n{valid_values}")
-
-
+@_format_args
 @lru_cache(8)
-def list_layers(variables: str or list = None, ssp: str or list = None, time_period: str = None, dataframe_response: bool = True, _include_allDatasets: bool = False) -> pd.DataFrame or list:
+def list_layers(variables: str or list = None, ssp: str or list = None, time_period: str = None, dataframe: bool = True, _include_allDatasets: bool = False) -> pd.DataFrame or list:
     """
     Lists available layers in the Bio-ORACLE server.
 
         variables (str|list): variables to filter from. Valid values are ['po4','o2','si','ph','sws','phyc','so','thetao','dfe','no3','sithick','tas','siconc','chl','mlotst','clt','terrain'].
         ssp (str|list): future scenario to choose from. Valid values are ['ssp119', 'ssp126', 'ssp370', 'ssp585', 'ssp460', 'ssp245', 'baseline'].
         time_period (str): time period to choose from. Valie values are either 'present' or 'future'.
-        dataframe_response (bool): whether to return a Pandas Dataframe. If False, will return a list.
+        dataframe (bool): whether to return a Pandas DataFrame. If False, will return a list.
     """
     valid_variables = ['po4','o2','si','ph','sws','phyc','so','thetao','dfe','no3','sithick','tas','siconc','chl','mlotst','clt','terrain']
     valid_ssp = ['ssp119', 'ssp126', 'ssp370', 'ssp585', 'ssp460', 'ssp245', 'baseline']
@@ -130,24 +151,24 @@ def list_layers(variables: str or list = None, ssp: str or list = None, time_per
     for arg in "variables", "ssp", "time_period":
         _validate_argument(arg, eval(arg), eval(f"valid_{arg}"))
 
-    dataframe = _layer_dataframe(_include_allDatasets)
+    _dataframe = _layer_dataframe(_include_allDatasets)
     # Filter resulting dataframe
     if variables:
-        dataframe = pd.concat([dataframe[dataframe["datasetID"].str.startswith(v.lower())] for v in variables])
+        _dataframe = pd.concat([_dataframe[_dataframe["datasetID"].str.startswith(v.lower())] for v in variables])
     if time_period:
-        start_decade = dataframe["datasetID"].str.split("_", expand=True)[3]
-        if time_period == "present":
-            return dataframe[start_decade == "2000"]
-        elif time_period == "future":
-            return dataframe[start_decade == "2020"]
+        start_decade = _dataframe["datasetID"].str.split("_", expand=True)[2]
+        if time_period == ("present",):
+            _dataframe = _dataframe[start_decade == "2000"]
+        elif time_period == ("future",):
+            _dataframe = _dataframe[start_decade == "2020"]
     if ssp:
-        dataframe = pd.concat([dataframe[(dataframe["datasetID"].str.contains(v.lower())) | (dataframe["datasetID"].str.contains(v.capitalize())) | (dataframe["datasetID"].str.contains(v.upper()))] for v in ssp])
+        _dataframe = pd.concat([_dataframe[(_dataframe["datasetID"].str.contains(v.lower())) | (_dataframe["datasetID"].str.contains(v.capitalize())) | (_dataframe["datasetID"].str.contains(v.upper()))] for v in ssp])
 
     # Convert to list
-    if dataframe_response:
-        return dataframe.reset_index(drop=True)
+    if dataframe:
+        return _dataframe.reset_index(drop=True)
     else:
-        return dataframe["datasetID"].to_list()
+        return _dataframe["datasetID"].to_list()
 
 
 def list_local_data():
