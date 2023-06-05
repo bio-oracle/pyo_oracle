5eacde603a8266a106181b3efb798cfd4ee977ab
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-06-05 19:12:28 +1000

    Remove default_server variable

--- pyo_oracle/__init__.py
+++ pyo_oracle/__init__.py
@@ -1,4 +1,4 @@
 from .main import download_layers, list_layers, list_local_data
-from .config import config, default_server
+from .config import config
 
 __all__ = ["download_layers", "list_layers", "list_local_data", "config"]
